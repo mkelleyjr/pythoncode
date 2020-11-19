57b71bc7b590f799ea8ce0e271464bd96bb41e7a
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2020-11-19 13:59:45 -0500

    Commit first file

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# Michael L. Keley Jr. 
+# November 19, 2020
+# hello_world.py
+# Hello World example in Python
+
+# the print() function prints text to the console
+# ("text to be written inside")
+
+print("Hello World from Python!")
+
+
