a704f7cc5197d15b19697a0baec0816ceee056e6
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2020-11-20 00:06:07 -0500

    addes some files

---
+++ pymath.py
@@ -0,0 +1,27 @@
+# Michael L. Kelley Jr.
+# November 20, 2020
+# pymath.py
+# Math examples in Python
+
+# python lets you do math quickly
+
+1 + 2 + 3 
+
+# addition 
+sum = 1 + 1
+print(sum)
+
+# subtraction 
+subtract = sum - 1
+print(subtract)
+
+# multiplication 
+mult = 5 * 5 * 5
+print(mult)
+
+# division
+div = 20 / 5
+print(div)
+
+
+
---
+++ variables.py
@@ -0,0 +1,19 @@
+# Michael L. Kelley Jr.
+# November 19, 2020
+# variables.py
+# variable example in Python
+
+# variables are used to store data (numbers, strings, arrays, etc.)
+
+# assign some variables with intergers and watch them get used 
+number_one = 5
+number_two = 5
+number_three = number_one + number_two 
+# print the answer out 
+print(number_three)
+
+# assign a variable with a string 
+name = "Michael"
+# print the string out 
+print(name)
+
