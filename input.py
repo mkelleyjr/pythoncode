42fef8511f716569fc32fe5c4fa6eada968c236c
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2020-11-21 01:25:40 -0500

    Added new file input.py

---
+++ input.py
@@ -0,0 +1,15 @@
+# Michael L. Kelley Jr.
+# November 21, 2020
+# input.py
+# simple input in Python
+
+print("Enter your number:")
+user_number = input()
+print(user_number)
+
+print("Enter your name:")
+user_name = input()
+print(user_name)
+
+
+
