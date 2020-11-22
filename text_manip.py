5e441fc4a234282bc745e9b3136afb8c8c554eda
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2020-11-22 17:33:19 -0500

    added text_manip.py

---
+++ text_manip.py
@@ -0,0 +1,20 @@
+# Micahel L. Kelley Jr.
+# November 22, 2020
+# text_manip.py
+# text manipulation in python - some examples
+
+# start by defining a string 
+my_string = "Welcome"
+print(my_string)
+
+# make it uppercase 
+upper = my_string.upper()
+print(upper)
+
+# make it lowercase 
+lower = my_string.lower()
+print(lower)
+
+# find the length of the string 
+length = len(my_string)
+print(length)
