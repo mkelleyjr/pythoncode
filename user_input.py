71a2650f2c78ac4dbc723e1987b6a074fb9e9710
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2020-11-26 00:45:49 -0500

    added user_input.py file.

---
+++ user_input.py
@@ -0,0 +1,16 @@
+# Michael L. Kelley Jr.
+# November 26, 2020
+# user_input.py
+# simple user input
+
+
+user_name = input("Hello! What is your name?")
+
+print("Hello " + user_name)
+
+user_age = input("What is your age?")
+
+print("You are " + user_age)
+
+
+
