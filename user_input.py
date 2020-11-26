a76b6ea59cec27fd657eea5fc0cea514b95ee0d6
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2020-11-26 00:46:22 -0500

    updated user_input.py

--- user_input.py
+++ user_input.py
@@ -3,11 +3,12 @@
 # user_input.py
 # simple user input
 
-
+# get the user's name 
 user_name = input("Hello! What is your name?")
 
 print("Hello " + user_name)
 
+# get the user's age 
 user_age = input("What is your age?")
 
 print("You are " + user_age)
